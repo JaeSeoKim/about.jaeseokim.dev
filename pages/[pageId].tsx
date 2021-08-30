fb914639a771f303fc588ff1f98df2330a5b4934
JaeSeoKim <devjaeseo@gmail.com>
2021-08-30 22:50:07 +0900

    chore: update ISR revalidate time

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -3,9 +3,14 @@ import { isDev, domain } from 'lib/config'
 import { getSiteMaps } from 'lib/get-site-maps'
 import { resolveNotionPage } from 'lib/resolve-notion-page'
 import { NotionPage } from 'components'
+import { GetStaticPropsContext } from 'next'
 
-export const getStaticProps = async (context) => {
-  const rawPageId = context.params.pageId as string
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{
+    pageId: string
+  }>
+) => {
+  const rawPageId = context.params.pageId
 
   try {
     if (rawPageId === 'sitemap.xml' || rawPageId === 'robots.txt') {
@@ -18,7 +23,7 @@ export const getStaticProps = async (context) => {
 
     const props = await resolveNotionPage(domain, rawPageId)
 
-    return { props, revalidate: 10 }
+    return { props, revalidate: 64 }
   } catch (err) {
     console.error('page error', domain, rawPageId, err)
 
